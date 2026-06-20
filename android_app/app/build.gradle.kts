f4c2498a848d70596652baa556bd56054232d999
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-06-21 02:59:01 +0600

    Update versionName to 1.5

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 5
-        versionName = "1.4"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
