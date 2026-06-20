4a0b174e383a485c339b8c6c4a66b01f49696702
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-06-21 02:35:37 +0600

    Bump versionCode to 3 to fix Play Console error

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "net.affscash.android"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
