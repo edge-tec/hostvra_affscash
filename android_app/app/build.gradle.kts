29cfbc7ac0ff4be8a066b246b578d08f542bbb6a
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-06-21 02:31:19 +0600

    Bump versionCode to 2 and versionName to 1.1 to fix Google Play Console upload error

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "net.affscash.android"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
