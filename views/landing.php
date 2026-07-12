dd510323c0b5d1e4621ccf5dd750f1e3415e1bd6
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-07-12 07:40:01 +0600

    fix: Update Short Link url to shroo.link in landing footer

--- views/landing.php
+++ views/landing.php
@@ -1174,7 +1174,7 @@ try {
         <a href="/dashboard-disclaimers">Dashboard Disclaimers</a>
         <a href="/register/affiliate">Sign Up</a>
         <a href="/login">Login</a>
-        <a href="/login">Short Link</a>
+        <a href="https://shroo.link" target="_blank">Short Link</a>
       </div>
       <?php if ($_mobileAppUrl !== ''): ?>
       <div style="display:flex;justify-content:center;margin:22px 0 6px">
