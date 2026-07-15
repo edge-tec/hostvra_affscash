89d91f68c4607c9c3bff35138c039db4490884ae
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-07-15 21:49:33 +0600

    fix traffic source override logs query

--- controllers/admin/TrafficSourceOverrideReportController.php
+++ controllers/admin/TrafficSourceOverrideReportController.php
@@ -8,7 +8,7 @@ $offerId = (int)Helpers::get('offer_id');
 $limit = min((int)(Helpers::get('limit') ?: 500), 5000);
 
 $params = [date('Y-m-d 00:00:00', strtotime($from)), date('Y-m-d 23:59:59', strtotime($to))];
-$where  = ['c.clicked_at BETWEEN ? AND ?', 'c.source_override_applied = 1'];
+$where  = ['c.clicked_at BETWEEN ? AND ?', 'c.override_source IS NOT NULL'];
 
 if ($affId > 0) {
     $where[] = 'c.affiliate_id = ?';
@@ -22,7 +22,7 @@ if ($offerId > 0) {
 $whereStr = implode(' AND ', $where);
 
 $logs = Database::fetchAll(
-    "SELECT c.id, c.click_id, c.ip_address, c.original_source, c.source as override_source, c.clicked_at,
+    "SELECT c.id, c.click_id, c.ip_address, c.traffic_source as original_source, c.override_source, c.clicked_at,
             o.name as offer_name, 
             CONCAT(u.first_name, ' ', u.last_name) as aff_name, 
             af.affiliate_code,
