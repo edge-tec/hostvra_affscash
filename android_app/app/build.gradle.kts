101a45ba4ea6d510ecb52c7f4e43c3cb444c3cdf
Mizanur Rahman <mizanurrahmanedge@outlook.com>
2026-07-22 21:40:29 +0600

    Update app version to versionCode 12 and versionName 2.0.4

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "net.affscash.android"
         minSdk = 23
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0.3"
+        versionCode = 12
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
